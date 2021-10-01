0ec1ba4287d8a447b99a492cc1f7bef0026ab37f
refik-karic <refik.karic.dev@gmail.com>
2021-09-30 22:21:41 -0700

    Unroll common Mat4x4 loops to bump up perf.

--- Mat4x4.cpp
+++ Mat4x4.cpp
@@ -12,54 +12,64 @@ Mat4x4::Mat4x4(const Mat4x4& copy) {
 }
 
 void Mat4x4::Identity() {
-  for (size_t row = 0; row < Rows; row++) {
-    for (size_t col = 0; col < Columns; col++) {
-      if (row == col) {
-        mData[row][col] = 1.f;
-      }
-      else {
-        mData[row][col] = 0.f;
-      }
-    }
-  }
+  Clear();
+  mData[0][0] = 1.f;
+  mData[1][1] = 1.f;
+  mData[2][2] = 1.f;
+  mData[3][3] = 1.f;
 }
 
 void Mat4x4::Clear() {
-  for (size_t row = 0; row < Rows; row++) {
-    mData[row].Clear();
-  }
+  mData[0].Clear();
+  mData[1].Clear();
+  mData[2].Clear();
+  mData[3].Clear();
 }
 
 Mat4x4 Mat4x4::Transpose() const {
   Mat4x4 result;
+  
+  result[0][0] = mData[0][0];
+  result[0][1] = mData[1][0];
+  result[0][2] = mData[2][0];
+  result[0][3] = mData[3][0];
 
-  for (size_t row = 0; row < Rows; row++) {
-    for (size_t col = 0; col < Columns; col++) {
-      result[row][col] = mData[col][row];
-    }
-  }
+  result[1][0] = mData[0][1];
+  result[1][1] = mData[1][1];
+  result[1][2] = mData[2][1];
+  result[1][3] = mData[3][1];
+
+  result[2][0] = mData[0][2];
+  result[2][1] = mData[1][2];
+  result[2][2] = mData[2][2];
+  result[2][3] = mData[3][2];
+
+  result[3][0] = mData[0][3];
+  result[3][1] = mData[1][3];
+  result[3][2] = mData[2][3];
+  result[3][3] = mData[3][3];
 
   return result;
 }
 
 void Mat4x4::SetTranslation(const Vec3& translation) {
-  size_t lastColumn = Columns - 1;
-  for (size_t row = 0; row < 3; row++) {
-    mData[row][lastColumn] = translation[row];
-  }
+  mData[0][3] = translation[0];
+  mData[1][3] = translation[1];
+  mData[2][3] = translation[2];
 }
 
 void Mat4x4::SetTranslation(const Vec4& translation) {
-  size_t lastColumn = Columns - 1;
-  for (size_t row = 0; row < Columns; row++) {
-    mData[row][lastColumn] = translation[row];
-  }
+  mData[0][3] = translation[0];
+  mData[1][3] = translation[1];
+  mData[2][3] = translation[2];
+  mData[3][3] = translation[3];
 }
 
 void Mat4x4::SetScale(const Vec4& scale) {
-  for (size_t row = 0; row < Columns; row++) {
-    mData[row][row] = scale[row];
-  }
+  mData[0][0] = scale[0];
+  mData[1][1] = scale[1];
+  mData[2][2] = scale[2];
+  mData[3][3] = scale[3];
 }
 
 void Mat4x4::SetRotation(float angle, Axis axis) {
@@ -93,11 +103,10 @@ void Mat4x4::SetRotation(float angle, Axis axis) {
 
 Vec4 Mat4x4::ApplyTransform(const Vec4& domain) const {
   Vec4 codomainResult;
-
-  for (size_t row = 0; row < Rows; row++) {
-    codomainResult[row] = domain * mData[row];
-  }
-
+  codomainResult[0] = domain * mData[0];
+  codomainResult[1] = domain * mData[1];
+  codomainResult[2] = domain * mData[2];
+  codomainResult[3] = domain * mData[3];
   return codomainResult;
 }
 

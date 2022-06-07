bb44f93879002feec4fbce1ae9b78aca4e700ecf
Refik Karic <refik.karic.dev@gmail.com>
2022-06-06 21:20:31 -0700

    Add support for axis-angle and vector construction of Quaternion. Implement Quaternion addition.

--- Quaternion.cpp
+++ Quaternion.cpp
@@ -3,11 +3,43 @@
 #include <cmath>
 
 namespace ZSharp {
+Quaternion::Quaternion() {
+
+}
+
+Quaternion::Quaternion(const Quaternion& rhs) {
+  mAngles = rhs.mAngles;
+}
+
+void Quaternion::operator=(const Quaternion& rhs) {
+  if (this != &rhs) {
+    mAngles = rhs.mAngles;
+  }
+}
+
 Quaternion::Quaternion(float theta, const Vec3& axes) {
   mAngles[QuatAxis::W] = cosf(theta / 2.f);
   mAngles[QuatAxis::X] = sinf(theta / 2.f) * axes[0];
   mAngles[QuatAxis::Y] = sinf(theta / 2.f) * axes[1];
   mAngles[QuatAxis::Z] = sinf(theta / 2.f) * axes[2];
+}
+
+Quaternion::Quaternion(const Vec4& axes) {
+  mAngles = axes;
+}
+
+float Quaternion::operator[](size_t index) const {
+  return mAngles[index];
+}
+
+Quaternion Quaternion::operator+(const Quaternion& rhs) const {
+  const float realPart = mAngles[QuatAxis::W] + rhs[QuatAxis::W];
+  const float xVec = mAngles[QuatAxis::X] + rhs[QuatAxis::X];
+  const float yVec = mAngles[QuatAxis::Y] + rhs[QuatAxis::Y];
+  const float zVec = mAngles[QuatAxis::Z] + rhs[QuatAxis::Z];
+  
+  Quaternion result(Vec4(realPart, xVec, yVec, zVec));
+  return result;
 }
 
 Mat4x4 Quaternion::GetRotationMatrix() const {
--- Quaternion.h
+++ Quaternion.h
@@ -3,9 +3,12 @@
 #include "Mat4x4.h"
 #include "Vec3.h"
 #include "Vec4.h"
+#include "ZBaseTypes.h"
 
 namespace ZSharp {
-class Quaternion {
+
+// Note that angles are interpreted and stored internally as radians.
+class Quaternion final {
   public:
   enum QuatAxis {
     W,
@@ -14,7 +17,21 @@ class Quaternion {
     Z
   };
 
+  Quaternion();
+
+  Quaternion(const Quaternion& rhs);
+
+  void operator=(const Quaternion& rhs);
+
+  // Axis angle.
   Quaternion(float theta, const Vec3& axes);
+
+  // W, I, J, K initialization.
+  Quaternion(const Vec4& axes);
+
+  float operator[](size_t index) const;
+
+  Quaternion operator+(const Quaternion& rhs) const;
 
   Mat4x4 GetRotationMatrix() const;
 

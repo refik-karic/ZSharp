ff3d79dfc518b9aa15ea5ab666bc150385644810
Refik Karic <refik.karic.dev@gmail.com>
2026-06-13 12:22:27 -0400

    Add some more helper functions to Span.

--- Span.h
+++ Span.h
@@ -3,6 +3,8 @@
 #include "ZBaseTypes.h"
 #include "ZAssert.h"
 
+#include <type_traits>
+
 namespace ZSharp {
 
 template<typename T>
@@ -21,6 +23,17 @@ class Span final {
     Iterator operator++(int) {
       Iterator temp(*this);
       ++(*this);
+      return temp;
+    }
+
+    Iterator& operator--() {
+      mPtr--;
+      return *this;
+    }
+
+    Iterator operator--(int) {
+      Iterator temp(mPtr);
+      --(*this);
       return temp;
     }
 
@@ -56,6 +69,8 @@ class Span final {
 
   }
 
+  Span(Span&&) = delete;
+
   void operator=(const Span& rhs) {
     if (&rhs != this) {
       mData = rhs.mData;
@@ -71,6 +86,44 @@ class Span final {
   const T& operator[](size_t index) const {
     ZAssert(index < mSize);
     return mData[index];
+  }
+
+  bool operator==(const Span& rhs) const {
+    if (mSize != rhs.mSize) {
+      return false;
+    }
+
+    if constexpr (std::is_trivially_copyable_v<T> && std::has_unique_object_representations_v<T>) {
+      return memcmp(mData, rhs.mData, mSize * sizeof(T)) == 0;
+    }
+    else {
+      for (size_t i = 0; i < mSize; ++i) {
+        if (mData[i] != rhs.mData[i]) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+  }
+
+  bool operator!=(const Span& rhs) const {
+    if (mSize != rhs.mSize) {
+      return true;
+    }
+
+    if constexpr (std::is_trivially_copyable_v<T> && std::has_unique_object_representations_v<T>) {
+      return memcmp(mData, rhs.mData, mSize * sizeof(T)) != 0;
+    }
+    else {
+      for (size_t i = 0; i < mSize; ++i) {
+        if (mData[i] != rhs.mData[i]) {
+          return true;
+        }
+      }
+
+      return false;
+    }
   }
 
   T* GetData() {
@@ -91,6 +144,14 @@ class Span final {
 
   Iterator end() const {
     return Iterator(mData + mSize);
+  }
+
+  Iterator rbegin() const {
+    return Iterator(mData + mSize - 1);
+  }
+
+  Iterator rend() const {
+    return Iterator(mData - 1);
   }
 
   private:
